78e5f2eca6148524ffcd268db8842fa0869456f7
Sergio Pérez Fernández <sergioperez794@gmail.com>
2021-11-27 01:43:03 +0100

    Wayland - Added support for virtual connectors

--- sunshine/platform/linux/kmsgrab.cpp
+++ sunshine/platform/linux/kmsgrab.cpp
@@ -166,6 +166,8 @@ static std::uint32_t from_view(const std::string_view &string) {
   _CONVERT("HDMI-B"sv, HDMIB);
   _CONVERT("eDP"sv, eDP);
   _CONVERT("DSI"sv, DSI);
+  _CONVERT("HEADLESS"sv, VIRTUAL);
+  _CONVERT("Virtual"sv, VIRTUAL);
 
   BOOST_LOG(error) << "Unknown Monitor connector type ["sv << string << "]: Please report this to the Github issue tracker"sv;
   return DRM_MODE_CONNECTOR_Unknown;
